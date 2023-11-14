5dd708477fdeeb2acb3b995e7353679d1d6dca82
Vlad888555 <110382044+Vlad888555@users.noreply.github.com>
2023-11-14 16:48:17 +0600

    Update bot.py

--- bot.py
+++ bot.py
@@ -14,7 +14,7 @@ intents.message_content = True
 bot = commands.Bot(command_prefix='$', intents=intents)
 
 headers = {
-    "Accept": "*/*",
+    "Accept": "text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.7",
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/119.0.0.0 Safari/537.36"
 }
 
